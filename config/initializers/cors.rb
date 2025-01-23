017df585f88a2f04b2167f9a41d6c1a291d57155
Justin Cefai <skatemich@yahoo.com>
2025-01-23 11:55:41 -0800

    more ruby linting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' #update this to reflect project URL once live
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+    origins "http://localhost:3000" # update this to reflect project URL once live
+    resource "*", headers: :any, methods: [ :get, :post, :patch, :put, :delete, :options ]
   end
 end
-
