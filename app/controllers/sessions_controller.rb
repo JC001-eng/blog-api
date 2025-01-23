92b59f62e95f64afc7069a70a3b2a82c25b3a7c9
Justin Cefai <skatemich@yahoo.com>
2025-01-23 13:01:11 -0800

    add user id to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       token = JWT.encode({ user_id: user.id }, Rails.application.credentials.secret_key_base, "HS256")
-      render json: { token: token, username: user.username }, status: :ok
+      render json: { token: token, username: user.username, user_id: user.id }, status: :ok
     else
       render json: { error: "Invalid email or password" }, status: :unauthorized
     end
