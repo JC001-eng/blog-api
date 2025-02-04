884daf740b291c3e778f628ee9ed2bc0653394bc
Justin Cefai <skatemich@yahoo.com>
2025-02-03 16:26:15 -0800

    add uploads controller and update routes

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,11 @@
+class UploadsController < ApplicationController
+  def create
+    if params[:file].present?
+      uploaded_file = params[:file]
+      blob = ActiveStorage::Blob.create_and_upload!(io: uploaded_file, filename: uploaded_file.original_filename, content_type: uploaded_file.content_type)
+      render json: { url: url_for(blob) }, status: :created
+    else
+      render json: { error: "No file attached", error_code: "NO_FILE" }, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   Rails.application.routes.draw do
+    post "/uploads", to: "uploads#create"
     resources :posts, only: [ :index, :show, :create, :update, :destroy ]
     resources :users, only: [ :create ]
     resources :sessions, only: [ :create ]
